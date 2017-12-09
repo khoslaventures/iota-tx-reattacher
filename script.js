5545abce30710d7719aa5c84b8f7f9a7dd335b78
Akash Khosla <me@akashkhosla.com>
2017-12-09 14:33:28 -0800

    removed extraneous functionality

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-var genAddrId = genAddrId || undefined;
-clearInterval(genAddrId);
-var btnAddr = document.getElementById("reattach-btn");
+var reattachBtn = document.getElementById("reattach-btn");
 
 function genNewAddr()
 {
-    btnAddr.click();
-    setTimeout(function() { btnAddr.click(); }, 1000);
+    reattachBtn.click();
+    setTimeout(function() { reattachBtn.click(); }, 1000);
 }
 
-btnAddr.click();
-setTimeout(function() { btnAddr.click(); }, 1000);
-var genAddrId = setInterval(genNewAddr, 5000);
+reattachBtn.click();
+setTimeout(function() { reattachBtn.click(); }, 1000);
 
 function stop()
 {
     console.log("\n\n\nOK, will now stop after this final attach has been completed!\n\n\n");
-    clearInterval(genAddrId);
 }
